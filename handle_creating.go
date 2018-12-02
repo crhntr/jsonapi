41f4dffafa4162ccee3a4c20a88d617f6c1ec4a2
Christopher Hunter <hunter.christopher@icloud.com>
2018-12-01 16:03:02 -0800

    Create Types for Handleing Creating Resources

---
+++ handle_creating.go
@@ -0,0 +1,32 @@
+package jsonapi
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+type (
+	// CreateFunc defines how to handle a request to create a resource.
+	CreateFunc func(res CreateResponder, req *http.Request, idStr string)
+
+	// CreateResponder defines what to respond to a request to create a resource.
+	CreateResponder interface {
+		DataSetter
+		ErrorAppender
+	}
+
+	// CreateRequestData should be used to unmarshal create resource request
+	// bodies.
+	CreateRequestData struct {
+		ID            string          `json:"id,omitempty"`
+		Type          string          `json:"type"`
+		Attributes    json.RawMessage `json:"attributes,omitempty"`
+		Relationships Relationships   `json:"relationships,omitempty"`
+	}
+)
+
+// // UnmarshalAttributes is a convenience method to unmarshal attributes into
+// // a struct or map.
+// func (data CreateRequestData) UnmarshalAttributes(attributes interface{}) error {
+// 	return json.Unmarshal(data.Attributes, attributes)
+// }
