26015f73bf8234c14e35b5616af2b3952d22d8cf
Christopher Hunter <hunter.christopher@icloud.com>
2018-12-08 13:55:35 -0800

    Remove Commented Code

--- serve_mux_test.go
+++ serve_mux_test.go
@@ -499,14 +499,3 @@ func TestHandle_ServeHTTP_RequestMux_Creating(t *testing.T) {
 		}
 	})
 }
-
-// type statusRecorder struct {
-// 	http.ResponseWriter
-// 	status int
-// }
-//
-// func (rec *statusRecorder) WriteHeader(code int) {
-// 	rec.status = code
-// 	rec.ResponseWriter.WriteHeader(code)
-// 	panic(code)
-// }
